f51d603c0dc31e1cab4e7d778e7e9520b9780af0
Gladwin Mohlamonyane <gladwin.mohlamonyane@f-secure.com>
2021-08-11 14:34:18 +0200

    starting to add pipes

--- main.go
+++ main.go
@@ -6,14 +6,40 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"syscall"
 
 	"github.com/kr/pretty"
 	"github.com/tidwall/gjson"
 )
 
+const configDir = "/.config/weather-app/"
+const tempPipe = "weather-app-tempc"
+const tempFeel = "weather-app-feelc"
+
+func getTempPipePath() string {
+	home := os.Getenv("HOME")
+	path = home + configDir + tempPipe
+	fileInfo, err := os.Stat(path)
+	//if there is no file, we expect that the program will raise the error
+	if err == nil { //the file exists, cause we could stat it... or we don't have perms to read it
+		if (fileInfo.Mode & os.ModeNamedPipe) > 0 {
+			//the pipe exists... we can read it...
+			return path
+		} else {
+			log.Fatalf("Error: Temp Pipe File \"%s\" exists; however, it is not a named pipe... change source code to accomodate\n", path)
+		}
+	}
+	//we are assuming the file does not exist at this point... either that or we don't have permission to use the known one
+	err = syscall.Mkfifo(path, 0644) //owner can write, but everybody can read
+	if err != nil {
+		log.Fatalf("Error: %s\n", err)
+	} //if no errors occured, we made the temp pipe... or we already had it
+	return path
+}
+
 func main() {
 	home := os.Getenv("HOME")
-	file, err := ioutil.ReadFile(home + "/.config/weather-app/api.key")
+	file, err := ioutil.ReadFile(home + configDir + "api.key")
 	if err != nil {
 		log.Fatalf("Please ensure that the your API Key is included in a file named \"api.key\" under your $HOME/.config/weather-app directory\nError: %s\n")
 	}
@@ -30,5 +56,5 @@ func main() {
 	//at this point we know that the string is a JSON object
 	contentStr := string(content)
 	pretty.Printf("Current Temp: %s\n", gjson.Get(contentStr, "current.temp_c").String())
-	pretty.Printf("Current Feel: %s\n", gjson.Get(contentStr, "current.feelslike_c").String())
+	//write temp to pipe
 }
